c086f15df0dc3ac5efe7597d699c577cd1527ae3
Zach Taylor <zachtylr04@gmail.com>
2022-03-13 15:27:03 -0600

    Nested routing

---
+++ app/routes/app.tsx
@@ -0,0 +1,8 @@
+export default function App() {
+  return (
+    <div>
+      <h1>App</h1>
+      <p>This is the app page.</p>
+    </div>
+  );
+}
---
+++ app/routes/discover.tsx
@@ -0,0 +1,8 @@
+export default function Discover() {
+  return (
+    <div>
+      <h1>Discover</h1>
+      <p>This is the discover page.</p>
+    </div>
+  );
+}
