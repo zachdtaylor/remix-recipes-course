b51c8ed90a7723287f8d210f52f86ee34fcd87a6
Zach Taylor <zachtylr04@gmail.com>
2022-03-13 16:30:16 -0600

    The root route

--- app/root.tsx
+++ app/root.tsx
@@ -1,4 +1,5 @@
 import {
+  Link,
   Links,
   LiveReload,
   Meta,
@@ -9,7 +10,7 @@ import {
 import type { MetaFunction } from "remix";
 
 export const meta: MetaFunction = () => {
-  return { title: "Remix Recipes" };
+  return { title: "New Remix App" };
 };
 
 export default function App() {
@@ -22,6 +23,12 @@ export default function App() {
         <Links />
       </head>
       <body>
+        <nav>
+          <Link to="/">Home</Link>
+          <Link to="discover">Discover</Link>
+          <Link to="app">App</Link>
+          <Link to="settings">Settings</Link>
+        </nav>
         <Outlet />
         <ScrollRestoration />
         <Scripts />
