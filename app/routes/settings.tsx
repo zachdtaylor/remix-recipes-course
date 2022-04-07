4d7b2bbc892615e30df2ce978d8369d9a28d6a10
Zach Taylor <zachtylr04@gmail.com>
2022-04-06 22:42:49 -0600

    3.01 - Loaders

--- app/routes/settings.tsx
+++ app/routes/settings.tsx
@@ -1,10 +1,24 @@
-import { Link, Outlet } from "remix";
+import { Link, LoaderFunction, Outlet, useLoaderData, json } from "remix";
+
+export const loader: LoaderFunction = () => {
+  return json(
+    { message: "Hello, there!" },
+    {
+      status: 418,
+      headers: {
+        custom: "hey",
+      },
+    }
+  );
+};
 
 export default function Settings() {
+  const data = useLoaderData();
   return (
     <div>
       <h1>Settings</h1>
       <p>This is the settings page.</p>
+      <p>Message from loader: {data.message}</p>
       <nav>
         <Link to="app">App</Link>
         <Link to="profile">Profile</Link>
