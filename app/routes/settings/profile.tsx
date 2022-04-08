ef61ea98d57126871e89185e2665ff9c379dd335
Zach Taylor <zachtylr04@gmail.com>
2022-04-07 22:11:00 -0600

    3.03 - Why load at route boundaries

--- app/routes/settings/profile.tsx
+++ app/routes/settings/profile.tsx
@@ -1,8 +1,16 @@
+import { json, LoaderFunction, useLoaderData } from "remix";
+
+export const loader: LoaderFunction = () => {
+  return json({ message: "Yo" });
+};
+
 export default function Profile() {
+  const data = useLoaderData();
   return (
     <div>
       <h2>Profile Settings</h2>
       <p>These are the profile settings</p>
+      <p>Message: {data.message}</p>
     </div>
   );
 }
