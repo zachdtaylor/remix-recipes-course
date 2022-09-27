674cae0406717e4149dfb8603000f3cb51320de3
Zach Taylor <zachtylr04@gmail.com>
2022-09-26 22:56:23 -0600

    [Authentication Prerequisites] Signing Cookies in Remix

--- app/cookies.ts
+++ app/cookies.ts
@@ -1,6 +1,11 @@
 import { createCookie } from "react-router";
 
+if (typeof process.env.AUTH_COOKIE_SECRET !== "string") {
+  throw new Error("Missing env: AUTH_COOKIE_SECRET");
+}
+
 export const userIdCookie = createCookie("remix-recipes__userId", {
+  secrets: [process.env.AUTH_COOKIE_SECRET],
   httpOnly: true,
   secure: true,
 });
