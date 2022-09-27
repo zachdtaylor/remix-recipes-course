7212cd284a6ad4ca4d93de7212725e8a3c4b9dcb
Zach Taylor <zachtylr04@gmail.com>
2022-09-26 22:56:23 -0600

    5.11 - Signing Cookies in Remix

--- app/cookies.ts
+++ app/cookies.ts
@@ -1,6 +1,11 @@
 import { createCookie } from "@remix-run/node";
 
+if (typeof process.env.AUTH_COOKIE_SECRET !== "string") {
+  throw new Error("Missing env: AUTH_COOKIE_SECRET");
+}
+
 export const userIdCookie = createCookie("remix-recipes__userId", {
+  secrets: [process.env.AUTH_COOKIE_SECRET],
   httpOnly: true,
   secure: true,
 });
