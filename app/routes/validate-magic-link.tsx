25938fb11f837816f4e2fcde3f62427a3509f49d
Zach Taylor <zachtylr04@gmail.com>
2022-10-20 21:54:34 -0600

    6.08 - Parsing the Magic Link Payload

--- app/routes/validate-magic-link.tsx
+++ app/routes/validate-magic-link.tsx
@@ -1,6 +1,9 @@
-import { json } from "@remix-run/node";
+import { LoaderFunctionArgs, json } from "@remix-run/node";
+import { getMagicLinkPayload } from "~/magic-links.server";
 
-export function loader() {
+export function loader({ request }: LoaderFunctionArgs) {
+  const magicLinkPayload = getMagicLinkPayload(request);
+  console.log(magicLinkPayload);
   return json("ok");
 }
 
