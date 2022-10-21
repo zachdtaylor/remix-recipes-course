8f56bf1213ac75dcaf909f00a9a4031f8523396f
Zach Taylor <zachtylr04@gmail.com>
2022-10-20 21:54:34 -0600

    [Magic Link Authentication] Parsing the Magic Link Payload

--- app/magic-links.server.ts
+++ app/magic-links.server.ts
@@ -1,4 +1,5 @@
 import Cryptr from "cryptr";
+import { data } from "react-router";
 
 if (typeof process.env.MAGIC_LINK_SECRET !== "string") {
   throw new Error("Missing env: MAGIC_LINK_SECRET");
@@ -28,4 +29,34 @@ export function generateMagicLink(email: string, nonce: string) {
   url.pathname = "/validate-magic-link";
   url.searchParams.set("magic", encryptedPayload);
   return url.toString();
+}
+
+function isMagicLinkPayload(value: any): value is MagicLinkPayload {
+  return (
+    typeof value === "object" &&
+    typeof value.email === "string" &&
+    typeof value.nonce === "string" &&
+    typeof value.createdAt === "string"
+  );
+}
+
+function invalidMagicLink(message: string) {
+  return data({ message }, { status: 400 });
+}
+
+export function getMagicLinkPayload(request: Request) {
+  const url = new URL(request.url);
+  const magic = url.searchParams.get("magic");
+
+  if (typeof magic !== "string") {
+    throw invalidMagicLink("'magic' search parameter does not exist");
+  }
+
+  const magicLinkPayload = JSON.parse(cryptr.decrypt(magic));
+
+  if (!isMagicLinkPayload(magicLinkPayload)) {
+    throw invalidMagicLink("invalid magic link payload");
+  }
+
+  return magicLinkPayload;
 }
--- app/routes/validate-magic-link.tsx
+++ app/routes/validate-magic-link.tsx
@@ -1,3 +1,9 @@
-export function loader() {
-  return "ok";
+import { getMagicLinkPayload } from "~/magic-links.server";
+import { Route } from "./+types/validate-magic-link";
+import { data } from "react-router";
+
+export function loader({ request }: Route.LoaderArgs) {
+  const magicLinkPayload = getMagicLinkPayload(request);
+  console.log(magicLinkPayload);
+  return new Response("ok");
 }
