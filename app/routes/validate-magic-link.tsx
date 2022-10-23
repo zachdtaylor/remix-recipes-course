1c46e8c77e837ddb3ff42312684ad6eee304e04a
Zach Taylor <zachtylr04@gmail.com>
2022-10-23 00:34:10 -0600

    6.11 - Finishing the Login

--- app/routes/validate-magic-link.tsx
+++ app/routes/validate-magic-link.tsx
@@ -1,12 +1,12 @@
-import { LoaderFunctionArgs, json } from "@remix-run/node";
+import { LoaderFunctionArgs, json, redirect } from "@remix-run/node";
 import { getMagicLinkPayload, invalidMagicLink } from "~/magic-links.server";
+import { getUser } from "~/models/user.server";
 import { commitSession, getSession } from "~/sessions";
 
 const magicLinkMaxAge = 1000 * 60 * 10; // 10 minutes
 
 export async function loader({ request }: LoaderFunctionArgs) {
   const magicLinkPayload = getMagicLinkPayload(request);
-  console.log(magicLinkPayload);
 
   // 1. Validate expiration time
   const createdAt = new Date(magicLinkPayload.createdAt);
@@ -22,6 +22,17 @@ export async function loader({ request }: LoaderFunctionArgs) {
 
   if (session.get("nonce") !== magicLinkPayload.nonce) {
     throw invalidMagicLink("invalid nonce");
+  }
+
+  const user = await getUser(magicLinkPayload.email);
+
+  if (user) {
+    session.set("userId", user.id);
+    return redirect("/app", {
+      headers: {
+        "Set-Cookie": await commitSession(session),
+      },
+    });
   }
 
   return json("ok", {
