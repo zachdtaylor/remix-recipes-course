aa320a2785bf607872fc4e9e698d623efa27b705
Zach Taylor <zachtylr04@gmail.com>
2022-12-15 22:26:09 -0700

    [The Recipes Page] Sorting the Recipe List

--- app/routes/app/recipes.tsx
+++ app/routes/app/recipes.tsx
@@ -25,6 +25,9 @@ export async function loader({ request }: Route.LoaderArgs) {
       },
     },
     select: { name: true, totalTime: true, imageUrl: true, id: true },
+    orderBy: {
+      createdAt: "desc",
+    },
   });
 
   return { recipes };
