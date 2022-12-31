61ba4dfbdab0f3bf53c9d2629ed58d377dfb9a44
Zach Taylor <zachtylr04@gmail.com>
2022-12-31 15:40:51 -0700

    8.21 - Rendering the Ingredients

--- app/routes/app/recipes/$recipeId.tsx
+++ app/routes/app/recipes/$recipeId.tsx
@@ -1,11 +1,23 @@
 import { json, LoaderArgs } from "@remix-run/node";
 import { Form, useLoaderData } from "@remix-run/react";
+import React from "react";
 import { ErrorMessage, Input } from "~/components/forms";
-import { TimeIcon } from "~/components/icons";
+import { TimeIcon, TrashIcon } from "~/components/icons";
 import db from "~/db.server";
 
 export async function loader({ params }: LoaderArgs) {
-  const recipe = await db.recipe.findUnique({ where: { id: params.recipeId } });
+  const recipe = await db.recipe.findUnique({
+    where: { id: params.recipeId },
+    include: {
+      ingredients: {
+        select: {
+          id: true,
+          amount: true,
+          name: true,
+        },
+      },
+    },
+  });
 
   return json({ recipe }, { headers: { "Cache-Control": "max-age=10" } });
 }
@@ -40,6 +52,36 @@ export default function RecipeDetail() {
           />
           <ErrorMessage></ErrorMessage>
         </div>
+      </div>
+      <div className="grid grid-cols-[30%_auto_min-content] my-4 gap-2">
+        <h2 className="font-bold text-sm pb-1">Amount</h2>
+        <h2 className="font-bold text-sm pb-1">Name</h2>
+        <div></div>
+        {data.recipe?.ingredients.map((ingredient) => (
+          <React.Fragment key={ingredient.id}>
+            <div>
+              <Input
+                type="text"
+                autoComplete="off"
+                name="ingredientAmount"
+                defaultValue={ingredient.amount ?? ""}
+              />
+              <ErrorMessage></ErrorMessage>
+            </div>
+            <div>
+              <Input
+                type="text"
+                autoComplete="off"
+                name="ingredientName"
+                defaultValue={ingredient.name}
+              />
+              <ErrorMessage></ErrorMessage>
+            </div>
+            <button>
+              <TrashIcon />
+            </button>
+          </React.Fragment>
+        ))}
       </div>
     </Form>
   );
