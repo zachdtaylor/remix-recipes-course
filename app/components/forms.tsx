c36b40c95961d772e5168eb5f2dabf38e04f8b94
Zach Taylor <zachtylr04@gmail.com>
2023-02-28 00:04:48 -0700

    10.05 - Creating some Components for the Modal

--- app/components/forms.tsx
+++ app/components/forms.tsx
@@ -127,3 +127,20 @@ export const Input = React.forwardRef<HTMLInputElement, InputProps>(
     );
   }
 );
+
+interface IconInputProps extends InputHTMLAttributes<HTMLInputElement> {
+  icon: React.ReactNode;
+}
+export function IconInput({ icon, ...props }: IconInputProps) {
+  return (
+    <div
+      className={classNames(
+        "flex items-stretch border-2 border-gray-300 rounded-md",
+        "focus-within:border-primary"
+      )}
+    >
+      <div className="px-2 flex flex-col justify-center">{icon}</div>
+      <input className="w-full py-3 px-2 outline-none rounded-md" {...props} />
+    </div>
+  );
+}
