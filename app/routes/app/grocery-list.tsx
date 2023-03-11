b852fab1b013bf199056d481135632abe7743d6c
Zach Taylor <zachtylr04@gmail.com>
2023-03-12 00:24:56 +0100

    10.16 - Defining the Grocery List Item Type

--- app/routes/app/grocery-list.tsx
+++ app/routes/app/grocery-list.tsx
@@ -1,3 +1,18 @@
+type GroceryListItem = {
+  id: string;
+  name: string;
+  uses: Array<{
+    id: string;
+    amount: string | null;
+    recipeName: string;
+    multiplier: number;
+  }>;
+};
+
+function GroceryListItem({ item }: { item: GroceryListItem }) {
+  return <div></div>;
+}
+
 export default function GroceryList() {
   return <div>Grocery List</div>;
 }
