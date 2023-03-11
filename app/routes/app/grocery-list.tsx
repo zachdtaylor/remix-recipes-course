29b79e54d9e55d108563c0277ddd5e9380356838
Zach Taylor <zachtylr04@gmail.com>
2023-03-12 00:41:32 +0100

    10.17 - Building the Grocery List Item Component

--- app/routes/app/grocery-list.tsx
+++ app/routes/app/grocery-list.tsx
@@ -1,3 +1,6 @@
+import { useFetcher } from "@remix-run/react";
+import { CheckCircleIcon } from "~/components/icons";
+
 type GroceryListItem = {
   id: string;
   name: string;
@@ -10,7 +13,31 @@ type GroceryListItem = {
 };
 
 function GroceryListItem({ item }: { item: GroceryListItem }) {
-  return <div></div>;
+  const fetcher = useFetcher();
+
+  return fetcher.state !== "idle" ? null : (
+    <div className="shadow-md rounded-md p-4 flex">
+      <div className="flex-grow">
+        <h1 className="text-sm font-bold mb-2 uppercase">{item.name}</h1>
+        <ul>
+          {item.uses.map((use) => (
+            <li key={use.id} className="py-1">
+              {use.amount} for {use.recipeName} (x{use.multiplier})
+            </li>
+          ))}
+        </ul>
+      </div>
+      <fetcher.Form method="post" className="flex flex-col justify-center">
+        <button
+          name="_action"
+          value="checkOffItem"
+          className="hover:text-primary"
+        >
+          <CheckCircleIcon />
+        </button>
+      </fetcher.Form>
+    </div>
+  );
 }
 
 export default function GroceryList() {
