df5f8cf816aeb7a4b7175ae0ac7cc01a10454e3a
Zach Taylor <zachtylr04@gmail.com>
2023-03-19 23:09:51 +0100

    10.25 - Clearing the Meal Plan

--- app/routes/app/recipes.tsx
+++ app/routes/app/recipes.tsx
@@ -10,7 +10,7 @@ import {
   useSearchParams,
   useTransition,
 } from "@remix-run/react";
-import { PrimaryButton, SearchBar } from "~/components/forms";
+import { DeleteButton, PrimaryButton, SearchBar } from "~/components/forms";
 import { CalendarIcon, PlusIcon } from "~/components/icons";
 import {
   RecipeCard,
@@ -54,21 +54,38 @@ export async function loader({ request }: LoaderArgs) {
 
 export async function action({ request }: ActionArgs) {
   const user = await requireLoggedInUser(request);
+  const formData = await request.formData();
 
-  const recipe = await db.recipe.create({
-    data: {
-      userId: user.id,
-      name: "New Recipe",
-      totalTime: "0 min",
-      imageUrl: "https://via.placeholder.com/150?text=Remix+Recipes",
-      instructions: "",
-    },
-  });
+  switch (formData.get("_action")) {
+    case "createRecipe": {
+      const recipe = await db.recipe.create({
+        data: {
+          userId: user.id,
+          name: "New Recipe",
+          totalTime: "0 min",
+          imageUrl: "https://via.placeholder.com/150?text=Remix+Recipes",
+          instructions: "",
+        },
+      });
 
-  const url = new URL(request.url);
-  url.pathname = `/app/recipes/${recipe.id}`;
+      const url = new URL(request.url);
+      url.pathname = `/app/recipes/${recipe.id}`;
 
-  return redirect(url.toString());
+      return redirect(url.toString());
+    }
+    case "clearMealPlan": {
+      await db.recipe.updateMany({
+        where: {
+          userId: user.id,
+        },
+        data: { mealPlanMultiplier: null },
+      });
+      return redirect("/app/recipes");
+    }
+    default: {
+      return null;
+    }
+  }
 }
 
 export default function Recipes() {
@@ -100,12 +117,26 @@ export default function Recipes() {
           </Link>
         </div>
         <Form method="post" className="mt-4" reloadDocument>
-          <PrimaryButton className="w-full">
-            <div className="flex w-full justify-center">
-              <PlusIcon />
-              <span className="ml-2">Create New Recipe</span>
-            </div>
-          </PrimaryButton>
+          {mealPlanOnlyFilterOn ? (
+            <DeleteButton
+              name="_action"
+              value="clearMealPlan"
+              className="w-full"
+            >
+              Clear Plan
+            </DeleteButton>
+          ) : (
+            <PrimaryButton
+              name="_action"
+              value="createRecipe"
+              className="w-full"
+            >
+              <div className="flex w-full justify-center">
+                <PlusIcon />
+                <span className="ml-2">Create New Recipe</span>
+              </div>
+            </PrimaryButton>
+          )}
         </Form>
         <ul>
           {data?.recipes.map((recipe) => {
