7c0d17e2a3ae1f17f643c655e599a27a94e90502
Zach Taylor <zachtylr04@gmail.com>
2023-04-02 23:06:12 +0200

    11.09 - Building the Form to Change the Site Theme

--- app/routes/settings/app.tsx
+++ app/routes/settings/app.tsx
@@ -1,8 +1,26 @@
+import { Form } from "@remix-run/react";
+import { PrimaryButton } from "~/components/forms";
+
 export default function App() {
   return (
-    <div>
-      <h2>App Settings</h2>
-      <p>These are the app settings</p>
-    </div>
+    <Form reloadDocument method="post">
+      <div className="mb-4 flex flex-col">
+        <label htmlFor="theme">Theme</label>
+        <select
+          id="theme"
+          name="theme"
+          className="p-2 mt-2 border-2 border-gray-200 rounded-md w-full md:w-64"
+          defaultValue="green"
+        >
+          <option value="red">Red</option>
+          <option value="orange">Orange</option>
+          <option value="yellow">Yellow</option>
+          <option value="green">Green</option>
+          <option value="blue">Blue</option>
+          <option value="purple">Purple</option>
+        </select>
+      </div>
+      <PrimaryButton>Save</PrimaryButton>
+    </Form>
   );
 }
