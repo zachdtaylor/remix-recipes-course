0efd97b1a6c081930e435310cc2e473d36dc5d3a
Zach Taylor <zachtylr04@gmail.com>
2023-06-30 17:45:52 -0600

    13.4 - Creating Dynamic Routes

--- remix.config.js
+++ remix.config.js
@@ -16,4 +16,16 @@ module.exports = {
   // serverBuildPath: "build/index.js",
   // publicPath: "/build/",
   // devServerPort: 8002
+  routes: (defineRoutes) =>
+    defineRoutes((route) => {
+      if (process.env.INCLUDE_TEST_ROUTES) {
+        if (process.env.NODE_ENV === "production") {
+          console.warn(
+            "WARNING: NODE_ENV is set to 'production', so we are going to skip creating test routes"
+          );
+          return;
+        }
+        route("__tests/login", "__test-routes__/login.tsx");
+      }
+    }),
 };
