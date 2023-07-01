5c21dc421df0a758c711ae127b858d524465a94b
Zach Taylor <zachtylr04@gmail.com>
2023-07-01 15:02:43 -0600

    13.5 - Implementing the Test Login Route

--- app/__test-routes__/login.tsx
+++ app/__test-routes__/login.tsx
@@ -1,3 +1,33 @@
-export async function loader() {
-  return "you're at the test login route!";
+import { LoaderArgs, redirect } from "@remix-run/node";
+import { createUser, getUser } from "~/models/user.server";
+import { commitSession, getSession } from "~/sessions";
+
+export async function loader({ request }: LoaderArgs) {
+  const url = new URL(request.url);
+  const firstName = url.searchParams.get("firstName");
+  const lastName = url.searchParams.get("lastName");
+  const email = url.searchParams.get("email");
+
+  if (!email) {
+    throw new Error("email is required to log in");
+  }
+
+  let user = await getUser(email);
+
+  if (!user) {
+    if (!firstName || !lastName) {
+      throw new Error("first and last name are required to log in");
+    }
+    user = await createUser(email, firstName, lastName);
+  }
+
+  const cookie = request.headers.get("cookie");
+  const session = await getSession(cookie);
+  session.set("userId", user.id);
+
+  return redirect("/app", {
+    headers: {
+      "Set-Cookie": await commitSession(session),
+    },
+  });
 }
