bbaed310a4612bf21b0f8abff3cdac770426dac6
Zach Taylor <zachtylr04@gmail.com>
2023-12-16 11:44:38 -0700

    4.09 - Building a Basic Shelf UI

--- app/routes/app/pantry.tsx
+++ app/routes/app/pantry.tsx
@@ -1,5 +1,6 @@
 import { json } from "@remix-run/node";
 import { useLoaderData } from "@remix-run/react";
+import classNames from "classnames";
 import { getAllShelves } from "~/models/pantry-shelf.server";
 
 export async function loader() {
@@ -11,10 +12,30 @@ export default function Pantry() {
   const data = useLoaderData<typeof loader>();
   return (
     <div>
-      <h1>Welcome to the pantry :)</h1>
-      <ul>
+      <ul
+        className={classNames(
+          "flex gap-8 overflow-x-auto",
+          "snap-x snap-mandatory md:snap-none"
+        )}
+      >
         {data.shelves.map((shelf) => (
-          <li key={shelf.id}>{shelf.name}</li>
+          <li
+            key={shelf.id}
+            className={classNames(
+              "border-2 border-primary rounded-md p-4 h-fit",
+              "w-[calc(100vw-2rem)] flex-none snap-center",
+              "md:w-96"
+            )}
+          >
+            <h1 className="text-2xl font-extrabold mb-2">{shelf.name}</h1>
+            <ul>
+              {shelf.items.map((item) => (
+                <li key={item.id} className="py-2">
+                  {item.name}
+                </li>
+              ))}
+            </ul>
+          </li>
         ))}
       </ul>
     </div>
