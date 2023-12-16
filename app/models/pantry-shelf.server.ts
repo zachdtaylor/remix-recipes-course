cd209f18cc7fd8c721fdffe984afaaffc39f1efe
Zach Taylor <zachtylr04@gmail.com>
2023-12-16 11:53:31 -0700

    [The Pantry Page] Intro to HTML Forms

--- app/models/pantry-shelf.server.ts
+++ app/models/pantry-shelf.server.ts
@@ -1,7 +1,13 @@
 import db from "~/db.server";
 
-export function getAllShelves() {
+export function getAllShelves(query: string | null) {
   return db.pantryShelf.findMany({
+    where: {
+      name: {
+        contains: query ?? "",
+        mode: "insensitive",
+      },
+    },
     include: {
       items: {
         orderBy: {
