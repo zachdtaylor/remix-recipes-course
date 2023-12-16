c57b0972b0db0d8d308e325a473a8fabfe4ae472
Zach Taylor <zachtylr04@gmail.com>
2023-12-16 10:21:53 -0700

    3.03 - Why Load Before Route Transitions

--- app/routes/settings/profile.tsx
+++ app/routes/settings/profile.tsx
@@ -1,8 +1,17 @@
+import { json } from "@remix-run/node";
+import { useLoaderData } from "@remix-run/react";
+
+export function loader() {
+  return json({ message: "Yo" });
+}
+
 export default function Profile() {
+  const data = useLoaderData<typeof loader>();
   return (
     <div>
       <h2>Profile Settings</h2>
       <p>These are the profile settings</p>
+      <p>Message: {data.message}</p>
     </div>
   );
 }
