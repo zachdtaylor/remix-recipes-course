6252be1cd83455db95e4bb3dd95a1615e60afae9
Zach Taylor <zachtylr04@gmail.com>
2023-12-15 21:05:38 -0700

    3.01 - What are Loaders?

--- app/routes/settings.tsx
+++ app/routes/settings.tsx
@@ -1,10 +1,17 @@
-import { Link, Outlet } from "@remix-run/react";
+import { json } from "@remix-run/node";
+import { Link, Outlet, useLoaderData } from "@remix-run/react";
+
+export function loader() {
+  return json({ message: "Hello, there!" });
+}
 
 export default function Settings() {
+  const data = useLoaderData<typeof loader>();
   return (
     <div>
       <h1>Settings</h1>
       <p>This is the settings page.</p>
+      <p>Message from loader: {data.message}</p>
       <nav>
         <Link to="app">App</Link>
         <Link to="profile">Profile</Link>
