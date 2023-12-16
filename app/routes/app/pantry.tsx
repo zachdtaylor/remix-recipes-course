704cd221aa41fe739a34f0f6589742dc7e079521
Zach Taylor <zachtylr04@gmail.com>
2023-12-16 14:21:35 -0700

    4.25 - Optimistically Creating Shelf Items

--- app/routes/app/pantry.tsx
+++ app/routes/app/pantry.tsx
@@ -168,6 +168,8 @@ function Shelf({ shelf }: ShelfProps) {
   const deleteShelfFetcher = useFetcher<any>();
   const saveShelfNameFetcher = useFetcher<any>();
   const createShelfItemFetcher = useFetcher<any>();
+  const createItemFormRef = React.useRef<HTMLFormElement>(null);
+  const { renderedItems, addItem } = useOptimisticItems(shelf.items);
 
   const isDeletingShelf =
     deleteShelfFetcher.formData?.get("_action") === "deleteShelf" &&
@@ -211,7 +213,28 @@ function Shelf({ shelf }: ShelfProps) {
           {saveShelfNameFetcher.data?.errors?.shelfId}
         </ErrorMessage>
       </saveShelfNameFetcher.Form>
-      <createShelfItemFetcher.Form method="post" className="flex py-2">
+      <createShelfItemFetcher.Form
+        method="post"
+        className="flex py-2"
+        ref={createItemFormRef}
+        onSubmit={(event) => {
+          const target = event.target as HTMLFormElement;
+          const itemNameInput = target.elements.namedItem(
+            "itemName"
+          ) as HTMLInputElement;
+          addItem(itemNameInput.value);
+          event.preventDefault();
+          createShelfItemFetcher.submit(
+            {
+              itemName: itemNameInput.value,
+              shelfId: shelf.id,
+              _action: "createShelfItem",
+            },
+            { method: "post" }
+          );
+          createItemFormRef.current?.reset();
+        }}
+      >
         <div className="w-full mb-2">
           <input
             type="text"
@@ -240,7 +263,7 @@ function Shelf({ shelf }: ShelfProps) {
         </ErrorMessage>
       </createShelfItemFetcher.Form>
       <ul>
-        {shelf.items.map((item) => (
+        {renderedItems.map((item) => (
           <ShelfItem shelfItem={item} key={item.id} />
         ))}
       </ul>
@@ -263,10 +286,7 @@ function Shelf({ shelf }: ShelfProps) {
 }
 
 type ShelfItemProps = {
-  shelfItem: {
-    id: string;
-    name: string;
-  };
+  shelfItem: RenderedItem;
 };
 function ShelfItem({ shelfItem }: ShelfItemProps) {
   const deleteShelfItemFetcher = useFetcher<any>();
@@ -275,9 +295,11 @@ function ShelfItem({ shelfItem }: ShelfItemProps) {
     <li className="py-2">
       <deleteShelfItemFetcher.Form method="post" className="flex">
         <p className="w-full">{shelfItem.name}</p>
-        <button name="_action" value="deleteShelfItem">
-          <TrashIcon />
-        </button>
+        {shelfItem.isOptimistic ? null : (
+          <button name="_action" value="deleteShelfItem">
+            <TrashIcon />
+          </button>
+        )}
         <input type="hidden" name="itemId" value={shelfItem.id} />
         <ErrorMessage className="pl-2">
           {deleteShelfItemFetcher.data?.errors?.itemId}
@@ -285,4 +307,39 @@ function ShelfItem({ shelfItem }: ShelfItemProps) {
       </deleteShelfItemFetcher.Form>
     </li>
   );
+}
+
+type RenderedItem = {
+  id: string;
+  name: string;
+  isOptimistic?: boolean;
+};
+function useOptimisticItems(savedItems: Array<RenderedItem>) {
+  const [optimisticItems, setOptimisticItems] = React.useState<
+    Array<RenderedItem>
+  >([]);
+
+  const renderedItems = [...optimisticItems, ...savedItems];
+
+  renderedItems.sort((a, b) => {
+    if (a.name === b.name) return 0;
+    return a.name < b.name ? -1 : 1;
+  });
+
+  React.useLayoutEffect(() => {
+    setOptimisticItems([]);
+  }, [savedItems]);
+
+  const addItem = (name: string) => {
+    setOptimisticItems((items) => [
+      ...items,
+      { id: createItemId(), name, isOptimistic: true },
+    ]);
+  };
+
+  return { renderedItems, addItem };
+}
+
+function createItemId() {
+  return `${Math.round(Math.random() * 1_000_000)}`;
 }
