a40a5c54de5f1a649afde4bfef5650c92210de7c
Zach Taylor <zachtylr04@gmail.com>
2023-12-16 11:33:24 -0700

    4.07 - Loading Data into the Pantry

--- app/routes/app/pantry.tsx
+++ app/routes/app/pantry.tsx
@@ -1,7 +1,23 @@
+import { PrismaClient } from "@prisma/client";
+import { json } from "@remix-run/node";
+import { useLoaderData } from "@remix-run/react";
+
+export async function loader() {
+  const db = new PrismaClient();
+  const shelves = await db.pantryShelf.findMany();
+  return json({ shelves });
+}
+
 export default function Pantry() {
+  const data = useLoaderData<typeof loader>();
   return (
     <div>
       <h1>Welcome to the pantry :)</h1>
+      <ul>
+        {data.shelves.map((shelf) => (
+          <li key={shelf.id}>{shelf.name}</li>
+        ))}
+      </ul>
     </div>
   );
 }
