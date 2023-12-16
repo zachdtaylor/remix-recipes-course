3d19a4775e813c62ebbb75f2265cdb8bf027478e
Zach Taylor <zachtylr04@gmail.com>
2023-12-16 11:53:31 -0700

    [The Pantry Page] Intro to HTML Forms

--- app/routes/app/pantry.tsx
+++ app/routes/app/pantry.tsx
@@ -1,19 +1,42 @@
 import classNames from "classnames";
-import { useLoaderData } from "react-router";
+import { useLoaderData, useSearchParams } from "react-router";
 import { getAllShelves } from "~/models/pantry-shelf.server";
+import { Route } from "./+types/pantry";
+import { SearchIcon } from "~/components/icons";
 
-export async function loader() {
-  const shelves = await getAllShelves();
+export async function loader({ request }: Route.LoaderArgs) {
+  const url = new URL(request.url);
+  const q = url.searchParams.get("q");
+  const shelves = await getAllShelves(q);
   return { shelves };
 }
 
 export default function Pantry() {
   const data = useLoaderData<typeof loader>();
+  const [searchParams] = useSearchParams();
   return (
     <div>
+      <form
+        className={classNames(
+          "flex border-2 border-gray-300 rounded-md",
+          "focus-within:border-primary md:w-80"
+        )}
+      >
+        <button className="px-2 mr-1">
+          <SearchIcon />
+        </button>
+        <input
+          defaultValue={searchParams.get("q") ?? ""}
+          type="text"
+          name="q"
+          autoComplete="off"
+          placeholder="Search Shelves..."
+          className="w-full py-3 px-2 outline-none rounded-md"
+        />
+      </form>
       <ul
         className={classNames(
-          "flex gap-8 overflow-x-auto",
+          "flex gap-8 overflow-x-auto mt-4",
           "snap-x snap-mandatory md:snap-none"
         )}
       >
