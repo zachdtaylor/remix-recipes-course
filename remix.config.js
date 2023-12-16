07f3a6fe2d99854663864f4d432eb429a408514b
Zach Taylor <zachtylr04@gmail.com>
2023-12-15 20:28:11 -0700

    1.02 - Creating a Remix Project

--- remix.config.js
+++ remix.config.js
@@ -1,11 +1,8 @@
-/**
- * @type {import('@remix-run/dev').AppConfig}
- */
-module.exports = {
-  ignoredRouteFiles: [".*"]
+/** @type {import('@remix-run/dev').AppConfig} */
+export default {
+  ignoredRouteFiles: ["**/.*"],
   // appDirectory: "app",
   // assetsBuildDirectory: "public/build",
-  // serverBuildPath: "build/index.js",
   // publicPath: "/build/",
-  // devServerPort: 8002
+  // serverBuildPath: "build/index.js",
 };
