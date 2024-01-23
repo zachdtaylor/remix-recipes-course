13a1b7f438a75284c8b97fd68c931a5a0766589b
Zach Taylor <zachtylr04@gmail.com>
2024-01-22 20:25:14 -0700

    8.11 - The Create Recipe Form

--- app/routes/app/recipes.tsx
+++ app/routes/app/recipes.tsx
@@ -1,6 +1,12 @@
-import { json, LoaderFunctionArgs } from "@remix-run/node";
-import { NavLink, Outlet, useLoaderData } from "@remix-run/react";
-import { SearchBar } from "~/components/forms";
+import {
+  ActionFunctionArgs,
+  json,
+  LoaderFunctionArgs,
+  redirect,
+} from "@remix-run/node";
+import { Form, NavLink, Outlet, useLoaderData } from "@remix-run/react";
+import { PrimaryButton, SearchBar } from "~/components/forms";
+import { PlusIcon } from "~/components/icons";
 import {
   RecipeCard,
   RecipeDetailWrapper,
@@ -29,6 +35,22 @@ export async function loader({ request }: LoaderFunctionArgs) {
   return json({ recipes });
 }
 
+export async function action({ request }: ActionFunctionArgs) {
+  const user = await requireLoggedInUser(request);
+
+  const recipe = await db.recipe.create({
+    data: {
+      userId: user.id,
+      name: "New Recipe",
+      totalTime: "0 min",
+      imageUrl: "https://via.placeholder.com/150?text=Remix+Recipes",
+      instructions: "",
+    },
+  });
+
+  return redirect(`/app/recipes/${recipe.id}`);
+}
+
 export default function Recipes() {
   const data = useLoaderData<typeof loader>();
 
@@ -36,6 +58,14 @@ export default function Recipes() {
     <RecipePageWrapper>
       <RecipeListWrapper>
         <SearchBar placeholder="Search Recipes..." />
+        <Form method="post" className="mt-4" reloadDocument>
+          <PrimaryButton className="w-full">
+            <div className="flex w-full justify-center">
+              <PlusIcon />
+              <span className="ml-2">Create New Recipe</span>
+            </div>
+          </PrimaryButton>
+        </Form>
         <ul>
           {data?.recipes.map((recipe) => (
             <li className="my-4" key={recipe.id}>
