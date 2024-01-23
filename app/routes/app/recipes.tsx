bc58ecd2be18375226724027f50d1e80b5c8ead6
Zach Taylor <zachtylr04@gmail.com>
2024-01-22 20:06:35 -0700

    8.07 - The Recipes Loader

--- app/routes/app/recipes.tsx
+++ app/routes/app/recipes.tsx
@@ -1,3 +1,18 @@
+import { json, LoaderFunctionArgs } from "@remix-run/node";
+import db from "~/db.server";
+import { requireLoggedInUser } from "~/utils/auth.server";
+
+export async function loader({ request }: LoaderFunctionArgs) {
+  const user = await requireLoggedInUser(request);
+
+  const recipes = await db.recipe.findMany({
+    where: { userId: user.id },
+    select: { name: true, totalTime: true, imageUrl: true, id: true },
+  });
+
+  return json({ recipes });
+}
+
 export default function Recipes() {
   return <div>Recipes</div>;
 }
