32a8fab0c60b871bf30394cdb4c01b9ba2dca9c3
Zach Taylor <zachtylr04@gmail.com>
2024-01-25 21:04:45 -0700

    [The Recipe Page] Displaying the Name and Total Time

--- app/routes/app/recipes/$recipeId.tsx
+++ app/routes/app/recipes/$recipeId.tsx
@@ -1,6 +1,8 @@
 import db from "~/db.server";
 import { Route } from "./+types/$recipeId";
-import { data, useLoaderData } from "react-router";
+import { data, Form, useLoaderData } from "react-router";
+import { ErrorMessage, Input } from "~/components/form";
+import { TimeIcon } from "~/components/icons";
 
 export function headers({ loaderHeaders }: Route.HeadersArgs) {
   return loaderHeaders;
@@ -16,8 +18,33 @@ export default function RecipeDetail() {
   const data = useLoaderData<typeof loader>();
 
   return (
-    <div>
-      <h1>{data.recipe?.name}</h1>
-    </div>
+    <Form method="post" reloadDocument>
+      <div className="mb-2">
+        <Input
+          key={data.recipe?.id}
+          type="text"
+          placeholder="Recipe Name"
+          autoComplete="off"
+          className="text-2xl font-extrabold"
+          name="name"
+          defaultValue={data.recipe?.name}
+        />
+        <ErrorMessage></ErrorMessage>
+      </div>
+      <div className="flex">
+        <TimeIcon />
+        <div className="ml-2 flex-grow">
+          <Input
+            key={data.recipe?.id}
+            type="text"
+            placeholder="Time"
+            autoComplete="off"
+            name="totalTime"
+            defaultValue={data.recipe?.totalTime}
+          />
+          <ErrorMessage></ErrorMessage>
+        </div>
+      </div>
+    </Form>
   );
 }
