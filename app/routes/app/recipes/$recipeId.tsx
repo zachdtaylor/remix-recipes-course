42f28405cc93210237f22aec9439075d6a06d0a9
Zach Taylor <zachtylr04@gmail.com>
2024-01-25 20:54:09 -0700

    8.12 - Setting Up the Recipe Detail Route

---
+++ app/routes/app/recipes/$recipeId.tsx
@@ -0,0 +1,19 @@
+import { LoaderFunctionArgs, json } from "@remix-run/node";
+import { useLoaderData } from "@remix-run/react";
+import db from "~/db.server";
+
+export async function loader({ params }: LoaderFunctionArgs) {
+  const recipe = await db.recipe.findUnique({ where: { id: params.recipeId } });
+
+  return json({ recipe });
+}
+
+export default function RecipeDetail() {
+  const data = useLoaderData<typeof loader>();
+
+  return (
+    <div>
+      <h1>{data.recipe?.name}</h1>
+    </div>
+  );
+}
