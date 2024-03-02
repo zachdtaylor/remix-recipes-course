2ca0f1e982e3bb9db24c9db69aa1051dae798b9f
Zach Taylor <zachtylr04@gmail.com>
2024-03-02 14:49:27 -0700

    11.02 - The PageLayout Component

--- app/routes/app.tsx
+++ app/routes/app.tsx
@@ -1,37 +1,14 @@
-import { NavLink as RemixNavLink, Outlet } from "@remix-run/react";
-import classNames from "classnames";
+import { PageLayout } from "~/components/layout";
 
 export default function App() {
   return (
-    <div className="flex flex-col h-full">
-      <h1 className="text-2xl font-bold my-4">App</h1>
-      <nav className="border-b-2 pb-2 mt-2">
-        <NavLink to="recipes">Recipes</NavLink>
-        <NavLink to="pantry">Pantry</NavLink>
-        <NavLink to="grocery-list">Grocery List</NavLink>
-      </nav>
-      <div className="py-4 overflow-y-auto">
-        <Outlet />
-      </div>
-    </div>
-  );
-}
-
-type NavLinkProps = {
-  to: string;
-  children: React.ReactNode;
-};
-function NavLink({ to, children }: NavLinkProps) {
-  return (
-    <RemixNavLink
-      to={to}
-      className={({ isActive }) =>
-        classNames("hover:text-gray-500 pb-2.5 px-2 md:px-4", {
-          "border-b-2 border-b-primary": isActive,
-        })
-      }
-    >
-      {children}
-    </RemixNavLink>
+    <PageLayout
+      title="App"
+      links={[
+        { to: "recipes", label: "Recipes" },
+        { to: "pantry", label: "Pantry" },
+        { to: "grocery-list", label: "Grocery List" },
+      ]}
+    />
   );
 }
