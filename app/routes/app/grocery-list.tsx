0216f5ed54586fbc2bf9d1b3d00dc76f15864565
Zach Taylor <zachtylr04@gmail.com>
2024-03-02 13:45:52 -0700

    10.18 - Getting Started on the Grocery List Loader

--- app/routes/app/grocery-list.tsx
+++ app/routes/app/grocery-list.tsx
@@ -1,5 +1,8 @@
+import { LoaderFunctionArgs } from "@remix-run/node";
 import { useFetcher } from "@remix-run/react";
 import { CheckCircleIcon } from "~/components/icons";
+import db from "~/db.server";
+import { requireLoggedInUser } from "~/utils/auth.server";
 
 type GroceryListItem = {
   id: string;
@@ -11,6 +14,38 @@ type GroceryListItem = {
     multiplier: number;
   }>;
 };
+
+function isMatch(ingredientName: string, pantryItemName: string) {
+  const lowerIngredientName = ingredientName.toLowerCase();
+  const lowerPantryItemName = pantryItemName.toLowerCase();
+  return lowerIngredientName === lowerPantryItemName;
+}
+
+export async function loader({ request }: LoaderFunctionArgs) {
+  const user = await requireLoggedInUser(request);
+
+  const ingredients = await db.ingredient.findMany({
+    where: {
+      recipe: {
+        userId: user.id,
+        mealPlanMultiplier: {
+          not: null,
+        },
+      },
+    },
+  });
+
+  const pantryItems = await db.pantryItem.findMany({
+    where: { userId: user.id },
+  });
+
+  const missingIngredients = ingredients.filter(
+    (ingredient) =>
+      !pantryItems.find((pantryItem) =>
+        isMatch(ingredient.name, pantryItem.name)
+      )
+  );
+}
 
 function GroceryListItem({ item }: { item: GroceryListItem }) {
   const fetcher = useFetcher();
