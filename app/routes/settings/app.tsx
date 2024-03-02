e7843b9a65999f2bcda0c584af619b3f9178102f
Zach Taylor <zachtylr04@gmail.com>
2024-03-02 15:04:49 -0700

    11.12 - The App Settings Route Loader

--- app/routes/settings/app.tsx
+++ app/routes/settings/app.tsx
@@ -1,9 +1,16 @@
-import { ActionFunctionArgs, json } from "@remix-run/node";
-import { Form } from "@remix-run/react";
+import { ActionFunctionArgs, LoaderFunctionArgs, json } from "@remix-run/node";
+import { Form, useLoaderData } from "@remix-run/react";
 import { z } from "zod";
 import { PrimaryButton } from "~/components/forms";
 import { themeCookie } from "~/cookies";
 import { validateForm } from "~/utils/validation";
+
+export async function loader({ request }: LoaderFunctionArgs) {
+  const cookieHeader = request.headers.get("cookie");
+  const theme = await themeCookie.parse(cookieHeader);
+
+  return { theme: typeof theme !== "string" ? "green" : theme };
+}
 
 const themeSchema = z.object({
   theme: z.string(),
@@ -24,6 +31,7 @@ export async function action({ request }: ActionFunctionArgs) {
 }
 
 export default function App() {
+  const data = useLoaderData<typeof loader>();
   return (
     <Form reloadDocument method="post">
       <div className="mb-4 flex flex-col">
@@ -32,7 +40,7 @@ export default function App() {
           id="theme"
           name="theme"
           className="p-2 mt-2 border-2 border-gray-200 rounded-md w-full md:w-64"
-          defaultValue="green"
+          defaultValue={data.theme}
         >
           <option value="red">Red</option>
           <option value="orange">Orange</option>
