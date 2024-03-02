bba7a7b192863974726711e01539f17f821c0200
Zach Taylor <zachtylr04@gmail.com>
2024-03-02 12:50:06 -0700

    [File Uploads] The Urlencoded Content Type

--- app/routes/app/recipes/$recipeId.tsx
+++ app/routes/app/recipes/$recipeId.tsx
@@ -319,7 +319,7 @@ export default function RecipeDetail({ params }: Route.ComponentProps) {
   };
 
   return (
-    <Form method="post" reloadDocument>
+    <Form method="post" encType="multipart/form-data" reloadDocument>
       <button name="_action" value="saveRecipe" className="hidden" />
       <div className="mb-2">
         <Input
