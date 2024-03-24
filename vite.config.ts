bf3abbd13a63fe81b12bf573271d1c5f857175f0
Zach Taylor <zachtylr04@gmail.com>
2024-03-24 12:39:38 -0600

    1.03 - The Vite Config File

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,22 @@
 import { vitePlugin as remix } from "@remix-run/dev";
 import { installGlobals } from "@remix-run/node";
+import { createRoutesFromFolders } from "@remix-run/v1-route-convention";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 installGlobals();
 
 export default defineConfig({
-  plugins: [remix(), tsconfigPaths()],
+  plugins: [
+    remix({
+      ignoredRouteFiles: ["**/.*"],
+      routes(defineRoutes) {
+        return createRoutesFromFolders(defineRoutes);
+      },
+    }),
+    tsconfigPaths(),
+  ],
+  server: {
+    port: 3000,
+  },
 });
