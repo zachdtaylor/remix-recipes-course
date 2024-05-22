b50b2a274c69cc12230bbb4299912d22579fac04
Zach Taylor <zachtylr04@gmail.com>
2024-05-21 21:09:04 -0600

    (Exercise) Revert changes from previous lesson

--- app/routes/settings.tsx
+++ app/routes/settings.tsx
@@ -1,20 +1,17 @@
 import { json } from "@remix-run/node";
-import { Link, Outlet, useRouteLoaderData } from "@remix-run/react";
-import { loader as childLoader } from "./settings/profile";
+import { Link, Outlet, useLoaderData } from "@remix-run/react";
 
 export function loader() {
   return json({ message: "Hello, there!" });
 }
 
 export default function Settings() {
-  const data = useRouteLoaderData<typeof childLoader>(
-    "routes/settings/profile"
-  );
+  const data = useLoaderData<typeof loader>();
   return (
     <div>
       <h1>Settings</h1>
       <p>This is the settings page.</p>
-      <p>Message from loader: {data?.message}</p>
+      <p>Message from loader: {data.message}</p>
       <nav>
         <Link to="app">App</Link>
         <Link to="profile">Profile</Link>
--- app/routes/settings/profile.tsx
+++ app/routes/settings/profile.tsx
@@ -1,18 +1,17 @@
 import { json } from "@remix-run/node";
-import { useRouteLoaderData } from "@remix-run/react";
-import { loader as parentLoader } from "../settings";
+import { useLoaderData } from "@remix-run/react";
 
 export function loader() {
   return json({ message: "Yo" });
 }
 
 export default function Profile() {
-  const data = useRouteLoaderData<typeof parentLoader>("routes/settings");
+  const data = useLoaderData<typeof loader>();
   return (
     <div>
       <h1>Profile Settings</h1>
       <p>These are the profile settings</p>
-      <p>Message: {data?.message}</p>
+      <p>Message: {data.message}</p>
     </div>
   );
 }
