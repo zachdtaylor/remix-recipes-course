45c173ee3fa0802e8edd55c7eb2a97da92baf191
Zach Taylor <zachtylr04@gmail.com>
2024-08-22 22:02:44 -0600

    Turn on unstable lazyRouteDiscovery flag
    This flag doesn't effect the app at all.

--- vite.config.ts
+++ vite.config.ts
@@ -28,6 +28,7 @@ export default defineConfig({
         v3_fetcherPersist: true,
         v3_relativeSplatPath: true,
         v3_throwAbortReason: true,
+        unstable_lazyRouteDiscovery: true,
       },
     }),
     tsconfigPaths(),
