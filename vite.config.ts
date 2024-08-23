ca6f81984ec6c136645d64fbf26e2a2206c4aab9
Zach Taylor <zachtylr04@gmail.com>
2024-08-22 21:34:35 -0600

    Turn on v3 feature flags
    These flags don'e effect the current app at all.
    
    Tthere's some potential to use the v3_fetcherPersist feature. I'm especially interested in the navigate=false flag on Forms. We could add a feature for drag-and-drop in the pantry using this flag.

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,7 @@
 import { vitePlugin as remix } from "@remix-run/dev";
-import { installGlobals } from "@remix-run/node";
 import { createRoutesFromFolders } from "@remix-run/v1-route-convention";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
-
-installGlobals();
 
 export default defineConfig({
   plugins: [
@@ -26,6 +23,11 @@ export default defineConfig({
             }
           }),
         };
+      },
+      future: {
+        v3_fetcherPersist: true,
+        v3_relativeSplatPath: true,
+        v3_throwAbortReason: true,
       },
     }),
     tsconfigPaths(),
