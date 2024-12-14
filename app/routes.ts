ff9ec4945c9de9fb9d2777ab3f7f1116c1e2f9a2
Zach Taylor <zachtylr04@gmail.com>
2024-12-14 15:42:06 -0700

    [Basic Routes and Styling] Basic Routing

--- app/routes.ts
+++ app/routes.ts
@@ -1,3 +1,8 @@
-import { type RouteConfig, index } from "@react-router/dev/routes";
+import { type RouteConfig, index, route } from "@react-router/dev/routes";
 
-export default [index("routes/home.tsx")] satisfies RouteConfig;
+export default [
+  index("routes/home.tsx"),
+  route("/discover", "routes/discover.tsx"),
+  route("/app", "routes/app.tsx"),
+  route("/settings", "routes/settings.tsx"),
+] satisfies RouteConfig;
---
+++ app/routes/settings.tsx
@@ -0,0 +1,10 @@
+import { Link } from "react-router";
+
+export default function Settings() {
+  return (
+    <div>
+      <h1>Settings</h1>
+      <p>This is the settings page</p>
+    </div>
+  );
+}
