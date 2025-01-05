484c17357c96b9f149ad124a82eb9c1e432267f8
Zach Taylor <zachtylr04@gmail.com>
2025-01-05 14:12:33 -0700

    [Data Loading] Pending UI when the Loader is Slow

--- app/routes/settings.tsx
+++ app/routes/settings.tsx
@@ -1,10 +1,15 @@
-import { Link, Outlet, useLoaderData } from "react-router";
+import React from "react";
+import { Await, Link, Outlet, useLoaderData, useLocation } from "react-router";
 
 export async function loader() {
-  return { message: "Hello, there!" };
+  const slowMessage = new Promise<string>((resolve) => {
+    setTimeout(() => resolve("This message is slow!"), 700);
+  });
+  return { message: "Hello, there!", slowMessage };
 }
 
 export default function Settings() {
+  const location = useLocation();
   const data = useLoaderData<typeof loader>();
 
   return (
@@ -12,6 +17,11 @@ export default function Settings() {
       <h1>Settings</h1>
       <p>This is the settings page</p>
       <p>Message from the loader: {data.message}</p>
+      <React.Suspense fallback={<div>Loading...</div>} key={location.pathname}>
+        <Await resolve={data.slowMessage}>
+          {(value) => <p>Another message: {value}</p>}
+        </Await>
+      </React.Suspense>
       <nav>
         <Link to="app">App</Link>
         <Link to="profile">Profile</Link>
