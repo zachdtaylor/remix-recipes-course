5e7d09c0a614f579345b25bfe32de07e93b77255
Zach Taylor <zachtylr04@gmail.com>
2025-01-05 12:47:49 -0700

    [Data Loading] What are Loaders?

--- app/routes/settings.tsx
+++ app/routes/settings.tsx
@@ -1,10 +1,17 @@
-import { Link, Outlet } from "react-router";
+import { Link, Outlet, useLoaderData } from "react-router";
+
+export async function loader() {
+  return { message: "Hello, there!" };
+}
 
 export default function Settings() {
+  const data = useLoaderData<typeof loader>();
+
   return (
     <div>
       <h1>Settings</h1>
       <p>This is the settings page</p>
+      <p>Message from the loader: {data.message}</p>
       <nav>
         <Link to="app">App</Link>
         <Link to="profile">Profile</Link>
