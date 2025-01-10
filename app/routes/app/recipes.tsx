5de5818729f9eb3d10030c8c1beb95c2a53d09c8
Zach Taylor <zachtylr04@gmail.com>
2025-01-10 16:01:39 -0700

    [The Recipes Page] The Recipes Loader

--- app/routes/app/recipes.tsx
+++ app/routes/app/recipes.tsx
@@ -1,3 +1,18 @@
+import { requireLoggedInUser } from "~/utils/auth.server";
+import { Route } from "./+types/recipes";
+import db from "~/db.server";
+
+export async function loader({ request }: Route.LoaderArgs) {
+  const user = await requireLoggedInUser(request);
+
+  const recipes = await db.recipe.findMany({
+    where: { userId: user.id },
+    select: { name: true, totalTime: true, imageUrl: true, id: true },
+  });
+
+  return { recipes };
+}
+
 export default function Recipes() {
   return <div>Recipes</div>;
 }
