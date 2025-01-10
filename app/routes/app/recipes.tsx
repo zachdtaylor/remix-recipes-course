51eae382bc736429329e8e7e1f743014826f719a
Zach Taylor <zachtylr04@gmail.com>
2025-01-10 16:07:17 -0700

    [The Recipes Page] Rendering the Recipe List

--- app/routes/app/recipes.tsx
+++ app/routes/app/recipes.tsx
@@ -1,6 +1,13 @@
 import { requireLoggedInUser } from "~/utils/auth.server";
 import { Route } from "./+types/recipes";
 import db from "~/db.server";
+import { NavLink, Outlet, useLoaderData } from "react-router";
+import {
+  RecipeCard,
+  RecipeDetailWrapper,
+  RecipeListWrapper,
+  RecipePageWrapper,
+} from "~/components/recipes";
 
 export async function loader({ request }: Route.LoaderArgs) {
   const user = await requireLoggedInUser(request);
@@ -14,5 +21,31 @@ export async function loader({ request }: Route.LoaderArgs) {
 }
 
 export default function Recipes() {
-  return <div>Recipes</div>;
+  const data = useLoaderData<typeof loader>();
+
+  return (
+    <RecipePageWrapper>
+      <RecipeListWrapper>
+        <ul>
+          {data?.recipes.map((recipe) => (
+            <li className="my-4" key={recipe.id}>
+              <NavLink reloadDocument to={recipe.id}>
+                {({ isActive }) => (
+                  <RecipeCard
+                    name={recipe.name}
+                    totalTime={recipe.totalTime}
+                    imageUrl={recipe.imageUrl}
+                    isActive={isActive}
+                  />
+                )}
+              </NavLink>
+            </li>
+          ))}
+        </ul>
+      </RecipeListWrapper>
+      <RecipeDetailWrapper>
+        <Outlet />
+      </RecipeDetailWrapper>
+    </RecipePageWrapper>
+  );
 }
