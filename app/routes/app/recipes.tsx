d2396ff6bb09108ff8f5194cfc886463f1726ea4
Zach Taylor <zachtylr04@gmail.com>
2025-01-18 19:28:29 -0700

    [The Grocery List] The Meal Plan Filter Backend

--- app/routes/app/recipes.tsx
+++ app/routes/app/recipes.tsx
@@ -37,6 +37,7 @@ export async function loader({ request }: Route.LoaderArgs) {
   const user = await requireLoggedInUser(request);
   const url = new URL(request.url);
   const q = url.searchParams.get("q");
+  const filter = url.searchParams.get("filter");
 
   const recipes = await db.recipe.findMany({
     where: {
@@ -45,6 +46,7 @@ export async function loader({ request }: Route.LoaderArgs) {
         contains: q ?? "",
         mode: "insensitive",
       },
+      mealPlanMultiplier: filter === "mealPlanOnly" ? { not: null } : {},
     },
     select: {
       name: true,
