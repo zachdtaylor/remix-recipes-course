03aca3dfea4841fcbbb5afe128522a93f9e97268
Zach Taylor <zachtylr04@gmail.com>
2025-08-02 13:43:15 -0600

    [The Pantry Page] Loading Data into the Pantry UI

--- app/routes/app/pantry.tsx
+++ app/routes/app/pantry.tsx
@@ -1,7 +1,22 @@
+import { PrismaClient } from "@prisma/client";
+import { useLoaderData } from "react-router";
+
+export async function loader() {
+  const db = new PrismaClient();
+  const shelves = await db.pantryShelf.findMany();
+  return { shelves };
+}
+
 export default function Pantry() {
+  const data = useLoaderData<typeof loader>();
   return (
     <div>
       <h1>Welcome to the pantry :)</h1>
+      <ul>
+        {data.shelves.map((shelf) => (
+          <li key={shelf.id}>{shelf.name}</li>
+        ))}
+      </ul>
     </div>
   );
 }
